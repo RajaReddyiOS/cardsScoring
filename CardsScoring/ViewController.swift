b1a170e41c6aff9187a8c5d3848fa8acb42abc71
rajareddy <raja@cogcons.com>
2019-01-28 11:28:39 +0530

    initial commit

---
+++ CardsScoring/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardsScoring
+//
+//  Created by Raja Reddy on 28/01/19.
+//  Copyright © 2019 Raja Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
